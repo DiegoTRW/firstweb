fab458fc6f827157f72d0489e9ba17a942884e94
Diego <diegoramesh07@gmail.com>
2024-05-09 12:08:13 -0600

    post app
    got the post app to work in html

--- pyweb/posts/views.py
+++ pyweb/posts/views.py
@@ -1,8 +1,7 @@
+
 from django.shortcuts import render
 from .models import Post
 
-# Create your views here.
-
 def post_list_v(request):
-    post_ob= Post.objects.all()
-    return render(request,'post_\post_list.html',{'post':post_ob})
+    posts = Post.objects.all()
+    return render(request, 'post_/post_list.html', {'posts': posts})
