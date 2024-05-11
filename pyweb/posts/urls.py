c4b2ff0af339271d0cf6be6ba9ed307f9b402c0d
Diego <diegoramesh07@gmail.com>
2024-05-11 16:49:52 -0400

    trying an failing

--- pyweb/posts/urls.py
+++ pyweb/posts/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.post_list_v),
+    path('',views.post_list_v,name="post"),
+    path('<slug:slug>',views.post_page,name="page")
 ]
--- pyweb/posts/views.py
+++ pyweb/posts/views.py
@@ -1,7 +1,12 @@
 
 from django.shortcuts import render
 from .models import Post
+from django.http import HttpResponse
 
 def post_list_v(request):
     posts = Post.objects.all()
     return render(request, 'post_/post_list.html', {'posts': posts})
+
+
+def post_page(request,slug):
+    return HttpResponse(slug)
